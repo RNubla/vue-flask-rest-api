eccaa1a021976248de317ebf5491f84261f1d3ed
RNubla <rnubla97@gmail.com>
2020-10-12 17:42:50 -0400

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,112 +1,112 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
+    <ul v-for="data in info" :key="data">
+      <li> Username: {{data.username}} First Name: {{data.first_name}} Last Name: {{data.last_name}}</li>
+      <!-- {{ data.first_name }}
+      {{ data.last_name }} -->
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <form method="post" @submit.prevent="checkForm">
+      <p v-if="errors.length">
+        <b>Please correct the following errors(s):</b>
+        <ul>
+          <li v-for="error in errors" :key="error">{{error}}</li>
+        </ul>
+      </p>
+      <p>
+        <label for="username">Username</label>
+        <input type="text" v-model="userForm.username" name="username" id="username">
+      </p>
+      <p>
+        <label for="password">Password</label>
+        <input type="password" v-model="userForm.password" name="password" id="password">
+      </p>
+      <p>
+        <label for="firstName">First Name</label>
+        <input type="text" v-model="userForm.first_name" name="firstName" id="firstName">
+      </p>
+      <p>
+        <label for="lastName">Last Name</label>
+        <input type="text" v-model="userForm.last_name" name="lastName" id="lastName">
+      </p>
+      <p>
+        <label for="age">Age</label>
+        <input type="text" v-model="userForm.age" name="age" id="age">
+      </p>
+      <p><input type="submit" value="Submit"></p>
+
+    </form>
   </div>
 </template>
 
 <script>
+const axios = require("axios");
+const API_URL = "http://localhost:5000/api";
 export default {
   name: "HelloWorld",
+  data() {
+    return {
+      errors: [],
+      usernameList: [],
+      info: null,
+      userForm: {
+        username: "",
+        password: "",
+        first_name: "",
+        last_name: "",
+        age: ""
+      }
+    };
+  },
   props: {
     msg: String
+  },
+  mounted() {
+    axios
+      .get(`${API_URL}/users`)
+      .then(res => {
+        this.info = res.data;
+      })
+      .catch(error => console.log(error));
+  },
+  methods: {
+    checkForm(e) {
+      if (
+        this.userForm.username &&
+        this.userForm.password &&
+        this.userForm.first_name &&
+        this.userForm.last_name &&
+        this.userForm.age
+      ) {
+        axios
+          .post(`${API_URL}/users`, this.userForm)
+          .then(res => {
+            res = this.userForm;
+            console.log(res);
+            // console.log(this.userForm);
+          })
+          .catch(error => console.log(error));
+        return true;
+      }
+      this.errors = [];
+
+      if (!this.userForm.username) {
+        this.errors.push("Username is required");
+      }
+      if (!this.userForm.password) {
+        this.errors.push("Password is required");
+      }
+      if (!this.userForm.first_name) {
+        this.errors.push("First Name is required");
+      }
+      if (!this.userForm.last_name) {
+        this.errors.push("Last Name is required");
+      }
+      if (!this.userForm.age) {
+        this.errors.push("Age is required");
+      }
+      e.preventDefault();
+    }
   }
 };
 </script>
